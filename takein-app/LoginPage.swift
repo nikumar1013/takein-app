39618dc693cdb3b68ce6b3f2fd321a11531738a5
Nikhil Kumar <nkumar@utexas.edu>
2022-03-15 19:27:45 -0500

    Setting up login page

---
+++ takein-app/LoginPage.swift
@@ -0,0 +1,18 @@
+//
+//  LoginPage.swift
+//  takein-app
+//
+//  Created by Nikhil Kumar on 3/15/22.
+//
+
+import UIKit
+
+class LoginPage: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
