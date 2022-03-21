7475e662ef1a94fac10450a0facb4ac5c70a8990
Akshay <akshay@Sachins-Air.grandenetworks.net>
2022-03-21 03:16:59 -0500

    got the scrollview on the create event page to dynamically be based on content height, also got the rectangles to be rounded + dynamically fit the text

--- takein-app/EventDetailsViewController.swift
+++ takein-app/EventDetailsViewController.swift
@@ -7,49 +7,38 @@
 
 import UIKit
 
-class EventDetailsCell: UITableViewCell {
-    
-    @IBOutlet weak var descriptionText: UILabel!
-}
-
 class EventDetailsViewController:
     
-    UIViewController, UITableViewDelegate, UITableViewDataSource {
+    UIViewController {
 
+//    @IBOutlet weak var eventTableView: ContentSizedTableView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var descriptionBox: UIStackView!
     
-    @IBOutlet weak var eventTableView: UITableView!
+    @IBOutlet weak var menuBox: UIStackView!
+    @IBOutlet weak var menuLabel: UILabel!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    
     let eventDescription = "Join us for an evening of Fall inspired pastries, tea, and cocktails.\nMy partner and I are new to the city, so we are throwing a small get together to hopefully get to know some other people!\nThere is no dress code, but something casual would be preferred. We own two dogs, so feel free to bring your pets! It’s gonna be a fun night, and if you want to bring anything to eat, please feel free.\nIf you are interested, please hit Claim Seat! We’d love to host you <3"
+    
+    let menu = "•\n•\n•\n•\n•\n•\n•\n•\n•\n•\n•\n•\n•\n•\n"
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        eventTableView.rowHeight = UITableView.automaticDimension
-        eventTableView.estimatedRowHeight = 600
-        eventTableView.delegate = self
-        eventTableView.dataSource = self
+        descriptionLabel.text = eventDescription
+
+        descriptionBox.layer.cornerRadius = 25;
+        descriptionBox.layer.masksToBounds = true;
+        
+        menuLabel.text = menu
+        menuBox.layer.cornerRadius = 25;
+        menuBox.layer.masksToBounds = true;
+        
         // Do any additional setup after loading the view.
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = eventTableView.dequeueReusableCell(withIdentifier: "DescriptionCell", for: indexPath) as! EventDetailsCell
-        cell.descriptionText.text = eventDescription
-        return cell
-    }
-    
-    
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
