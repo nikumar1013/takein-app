fb462b1cc48a3d96fcdf0a6f6c044d7b5737174a
Nick Wille <nickwille@Nicks-MacBook-Pro.local>
2022-04-03 17:48:05 -0500

    Finally got navbar workaround fixed + storyboard ready

--- takein-app/SuccessLoginSegue.swift
+++ takein-app/SuccessLoginSegue.swift
@@ -8,12 +8,12 @@
 import UIKit
 
 class SuccessLoginSegue: UIStoryboardSegue {
-    override func perform() {
+  /*  override func perform() {
         if let navigationController = source.navigationController as UINavigationController? {
             var controllers = navigationController.viewControllers
             controllers.removeAll()
             controllers.append(destination)
             navigationController.setViewControllers(controllers, animated: true)
         }
-    }
+    } */
 }
